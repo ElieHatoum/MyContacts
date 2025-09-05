b6284afc838762181ea20ca332089672485eb182
Elie Hatoum <elie.hattoum@gmail.com>
2025-09-05 16:23:26 +0200

    Clean up

--- server/src/models/user.model.js
+++ server/src/models/user.model.js
@@ -1,14 +1,15 @@
 const mongoose = require("mongoose");
+
 const userSchema = mongoose.Schema({
-  userId: {
-    type: String,
-  },
-  email: {
-    type: String,
-  },
-  password: {
-    type: String,
-  },
+    userId: {
+        type: String,
+    },
+    email: {
+        type: String,
+    },
+    password: {
+        type: String,
+    },
 });
 
 module.exports = mongoose.model("User", userSchema);
