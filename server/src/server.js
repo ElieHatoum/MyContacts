3138e74b00088809fbbae739b9bc2c20390f0a2d
Elie Hatoum <elie.hattoum@gmail.com>
2025-09-15 15:17:27 +0200

    Added Cors

--- server/src/server.js
+++ server/src/server.js
@@ -1,6 +1,7 @@
 require("dotenv").config({ path: "./.env" });
 
 const express = require("express");
+const cors = require("cors");
 const { connectDB } = require("./config/db.config.js");
 const { specs, swaggerUi } = require("./config/swagger.config.js");
 const auth = require("./routes/auth.routes.js");
@@ -9,6 +10,7 @@ const contacts = require("./routes/contacts.routes.js");
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(cors());
 app.use(express.json());
 app.use("/api", contacts);
 app.use("/api/auth", auth);
