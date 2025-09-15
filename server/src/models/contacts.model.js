5276e5ee0d8c72fd0a25102d709bcc6ab143b5e5
Elie Hatoum <elie.hattoum@gmail.com>
2025-09-15 10:35:43 +0200

    Added Schema rules

--- server/src/models/contacts.model.js
+++ server/src/models/contacts.model.js
@@ -1,10 +1,15 @@
 const mongoose = require("mongoose");
 
 const contactSchema = mongoose.Schema({
-    userId: { type: String },
+    userId: { type: String, immutable: true },
     firstName: { type: String },
     lastName: { type: String },
-    phoneNumber: { type: Number },
+    phoneNumber: {
+        type: String,
+        match: [/^[0-9]+$/, "Phone number should only contain digits"],
+        minLength: [10, "Phone number should have minimum 10 digits"],
+        maxLength: [20, "Phone number should have maximum 20 digits"],
+    },
 });
 
 module.exports = mongoose.model("Contact", contactSchema);
