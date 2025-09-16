d0e25135a89ca87417098f514c779f431e71144a
Elie Hatoum <elie.hattoum@gmail.com>
2025-09-16 17:17:10 +0200

    Clean up

--- client/src/components/bar.jsx
+++ client/src/components/bar.jsx
@@ -2,55 +2,12 @@ import AppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 import SearchIcon from "@mui/icons-material/Search";
-import { styled, alpha } from "@mui/material/styles";
-import InputBase from "@mui/material/InputBase";
 import { useNavigate } from "react-router-dom";
 import IconButton from "@mui/material/IconButton";
 import LogoutIcon from "@mui/icons-material/Logout";
 import Box from "@mui/material/Box";
 import AddIcon from "@mui/icons-material/Add";
-
-//Pour la recherche
-//Exemple de la doc de Material UI et puis instrumenter au besoin
-const Search = styled("div")(({ theme }) => ({
-    position: "relative",
-    borderRadius: theme.shape.borderRadius,
-    backgroundColor: alpha(theme.palette.common.white, 0.15),
-    "&:hover": {
-        backgroundColor: alpha(theme.palette.common.white, 0.25),
-    },
-    marginRight: theme.spacing(2),
-    marginLeft: 0,
-    width: "100%",
-    [theme.breakpoints.up("sm")]: {
-        marginLeft: theme.spacing(3),
-        width: "auto",
-    },
-}));
-
-const SearchIconWrapper = styled("div")(({ theme }) => ({
-    padding: theme.spacing(0, 2),
-    height: "100%",
-    position: "absolute",
-    pointerEvents: "none",
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "center",
-}));
-
-const StyledInputBase = styled(InputBase)(({ theme }) => ({
-    color: "inherit",
-    "& .MuiInputBase-input": {
-        padding: theme.spacing(1, 1, 1, 0),
-        // vertical padding + font size from searchIcon
-        paddingLeft: `calc(1em + ${theme.spacing(4)})`,
-        transition: theme.transitions.create("width"),
-        width: "100%",
-        [theme.breakpoints.up("md")]: {
-            width: "20ch",
-        },
-    },
-}));
+import { Button } from "@mui/material";
 
 function Bar({ onAddClick }) {
     const navigate = useNavigate();
@@ -69,23 +26,19 @@ function Bar({ onAddClick }) {
             <Toolbar>
                 <Typography variant="h6">Contacts</Typography>
 
-                <Search>
-                    <SearchIconWrapper>
-                        <SearchIcon />
-                    </SearchIconWrapper>
-                    <StyledInputBase
-                        placeholder="Search…"
-                        inputProps={{ "aria-label": "search" }}
-                    />
-                </Search>
-                <IconButton
+                <Button
                     size="large"
-                    edge="end"
-                    color="inherit"
+                    variant="contained"
+                    sx={{
+                        color: "#000000ff",
+                        backgroundColor: "white",
+                        flexGrow: 1,
+                        marginLeft: "20%",
+                    }}
                     onClick={() => onAddClick()}
                 >
-                    <AddIcon />
-                </IconButton>
+                    ADD
+                </Button>
 
                 <Box sx={{ flexGrow: 1 }} />
 
